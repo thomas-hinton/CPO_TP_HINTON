c117c14c8b087bca85efe25a8f9a008a125841c0
thomas hinton <thomas.hinton@epfedu.fr>
2022-10-28 14:48:33 +0200

    -Création de la classe "BouteilleBierre" -Remplissage de la classe -Ajout d'une nouvelle méthode "lireEtiquette"

---
+++ TP2_Bieres_HINTON/src/main/java/BouteilleBiere.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 33621
+ */
+public class BouteilleBiere {
+    String nom = "";
+    float degreAlcool = 0;
+    String brasserie = "";
+    boolean ouverte = false;
+
+public void lireEtiquette() {
+System.out.println("Bouteille de " + nom +" (" + degreAlcool +
+ " degres) \nBrasserie : " + brasserie ) ;
+}}
