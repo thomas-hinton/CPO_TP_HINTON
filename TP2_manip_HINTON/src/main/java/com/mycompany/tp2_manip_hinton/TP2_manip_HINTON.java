1e2cd7e384476f8a82a195b7031ff8c01a718f1c
thomas hinton <thomas.hinton@epfedu.fr>
2022-10-30 17:43:42 +0100

    Création de eux tartiflettes: -Créations de deux objet de class Tartiflette et de références respectives "assiette1" et assiette2. Initialisation de leur attributs -Création d'une deuxième référence "assiette3" de l'objet déja référencé "assiette2" -Afficge de chacune de ces référence, affichage de la valeur de leur attribut. On s'appercois que un même objet peut avoir deux références différentes  ( ici assiette2 et assiette3)

--- TP2_manip_HINTON/src/main/java/com/mycompany/tp2_manip_hinton/TP2_manip_HINTON.java
+++ TP2_manip_HINTON/src/main/java/com/mycompany/tp2_manip_hinton/TP2_manip_HINTON.java
@@ -12,6 +12,17 @@ package com.mycompany.tp2_manip_hinton;
 public class TP2_manip_HINTON {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //Création de 2 objet de class Tartiflette:
+        Tartiflette assiette1 = new Tartiflette(500) ;
+        Tartiflette assiette2 = new Tartiflette(600) ;
+        //Création d'une deuxieme référence à l'objet de référence Tartiflette2:
+        Tartiflette assiette3 = assiette2 ;
+        
+        //Affichage du nombre de callories de chaque assietes:
+        System.out.println("nb de calories de Assiette 1 : " + assiette1.nbCalories) ;
+        System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories) ;
+        System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories) ;  //On se rend compte que il existe réèlement que deux assietes. Assietes 2 et 3 renvoient vers la même assiete
+        
+        
     }
 }
