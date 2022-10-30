80abfed3c0e75d41f96709b73098b0cb0dba040b
thomas hinton <thomas.hinton@epfedu.fr>
2022-10-30 17:27:01 +0100

    Création/Définition de deux class "Tartiflette" et "Moussaka": -Définition des attributs de chacune de ces class ("nbCalories") -Définition d'un contructeur d'objet qui initialise l’attribut "nbCalories" avec une valeur passée en paramètre

---
+++ TP2_manip_HINTON/src/main/java/com/mycompany/tp2_manip_hinton/Moussaka.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tp2_manip_hinton;
+
+/**
+ *
+ * @author 33621
+ */
+//Création et définition de la classe "Moussaka"
+public class Moussaka {
+    int nbCalories;
+    
+    //Constructeur premettant de créer un objet de type "Moussaka et d'initialiser son attribut "nbCalories"
+    public Moussaka(int Calories){
+        nbCalories = Calories;
+    }
+}
---
+++ TP2_manip_HINTON/src/main/java/com/mycompany/tp2_manip_hinton/Tartiflette.java
@@ -0,0 +1,20 @@
+package com.mycompany.tp2_manip_hinton;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 33621
+ */
+//Création et définition de la classe "Tartiflette"
+public class Tartiflette {
+    int nbCalories;
+    
+    //Constructeur premettant de créer un objet de type "Moussaka et d'initialiser son attribut "nbCalories"
+    public Tartiflette(int Calories){
+        nbCalories = Calories;
+    }
+}
