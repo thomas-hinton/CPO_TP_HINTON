05d29ffb261d3bcc50e83554eb23643c91471b7c
thomas hinton <thomas.hinton@epfedu.fr>
2022-10-30 18:20:29 +0100

    Création du nouveau projet "TP2_relation_1_NOM1"

---
+++ TP2_relation_1_HINTON/src/main/java/com/mycompany/tp2_relation_1_hinton/TP2_relation_1_HINTON.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tp2_relation_1_hinton;
+
+/**
+ *
+ * @author 33621
+ */
+public class TP2_relation_1_HINTON {
+
+    public static void main(String[] args) {
+    }
+}
