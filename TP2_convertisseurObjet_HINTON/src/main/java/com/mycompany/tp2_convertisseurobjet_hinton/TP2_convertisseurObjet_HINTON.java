86fa64457a960b6539356a969c9f628f8e82b393
thomas hinton <thomas.hinton@epfedu.fr>
2022-10-30 16:48:05 +0100

    Création d'un MenuConversion pour l'utilisateur: -Création d'un nouvel objet appelé "MenuUtilisateur" de class "Convertisseur" -Création d'un nouvel objet appelé "sc" de class "Scanner" (permet de récupérer ce que renseigne l'utilisateur) -Création d'échanges avec l'utilisateur pour qu'il puisse renseigner sa température et la conversion souhaitée et que le programme lui renvoie enssuite sa valeur convertie -Ajout d'un test permettant de rensigner l'utilisateur sur sa saisie (Mauvaise ou bonne) Fin du "TP2_convertisseurObjet"

--- TP2_convertisseurObjet_HINTON/src/main/java/com/mycompany/tp2_convertisseurobjet_hinton/TP2_convertisseurObjet_HINTON.java
+++ TP2_convertisseurObjet_HINTON/src/main/java/com/mycompany/tp2_convertisseurobjet_hinton/TP2_convertisseurObjet_HINTON.java
@@ -5,6 +5,8 @@
 
 package com.mycompany.tp2_convertisseurobjet_hinton;
 
+import java.util.Scanner; //Importation de la class "Scanner" qui nous permet de récupérer une valeur de l'utilisateur
+
 /**
  *
  * @author 33621
@@ -23,6 +25,7 @@ public class TP2_convertisseurObjet_HINTON {
         //Affichage du nombre de conversions effectué par l'objet "ConvertisseurUniversel":
          System.out.println(ConvertisseurUniversel);
         
+         
         //Création de l'objet "ConvertisseurMeteo" de class "Convertisseur":
         Convertisseur ConvertisseurMeteo = new Convertisseur ();
         //Conversion avec l'aide de méthodes de class "Convertisseur"
@@ -30,5 +33,57 @@ public class TP2_convertisseurObjet_HINTON {
         ConvertisseurMeteo.FarenheitVersCelcius(75);
         //Affichage du nombre de conversions effectué par l'objet "ConvertisseurMeteo":
         System.out.println(ConvertisseurMeteo);
+        
+        
+        //Création de l'objet "MenuConversions" (une menu permettant de proposer à l'utilisateur de choisir une conversion):
+        Convertisseur MenuConversions = new Convertisseur();
+        //Création de l'objet "sc" (contiendra la valeur renvoyée par l'utilisateur)
+        Scanner sc; 
+        sc = new Scanner(System.in);
+        //Déclaration et initialisation de la Valeur de sortie
+        double ValeurSortie = 0;
+        
+        //Demande à l'utilisateur sa température à convertir et l'ajoute à une variable "ValeurEntree":
+        System.out.println("Veuillez renseigner la température à convertir:");
+        double ValeurEntree = sc.nextDouble();
+        
+        //Demande à l'utilisateur la onversion qu'il souhaite réaliser et l'ajoute à la variable "ConvChoisie":
+        System.out.println("""
+                           Veillez renseigner la conversion souhait\u00e9e(n\u00b0):
+                           1)Celcius en Kelvin
+                           2)Kelvin en Celcius
+                           3)Farenheit en Celcius
+                           4)Celcius en Farenheit
+                           5)Farenheit vers Kelvin
+                           6)Kelvin vers Farenheit""");
+        double ConvChoisie = sc.nextInt();
+        
+        //converti avec la méthode choisie par l'utilisateur:
+        if (ConvChoisie == 1){
+            ValeurSortie = MenuConversions.CelciusVersKelvin(ValeurEntree);  
+        }
+        if (ConvChoisie == 2){
+            ValeurSortie = MenuConversions.KelvinVersCelcius(ValeurEntree);
+        }
+        if (ConvChoisie == 3){
+            ValeurSortie = MenuConversions.FarenheitVersCelcius(ValeurEntree);
+        }
+        if (ConvChoisie == 4){
+            ValeurSortie = MenuConversions.CelciusVersFarenheit(ValeurEntree);
+        }
+        if (ConvChoisie == 5){
+            ValeurSortie = MenuConversions.FarenheitVersKelvin(ValeurEntree);
+        }
+        if (ConvChoisie == 6){
+            ValeurSortie = MenuConversions.KelvinVersFarenheit(ValeurEntree);
+        }
+        
+        //Vérifie si il y une erreur dans la saisie de la méthode choisie / renvoie le résultat de la conversion à l'utilisateur:
+        if (ConvChoisie != 1 || ConvChoisie !=2 || ConvChoisie !=3 || ConvChoisie != 4 || ConvChoisie !=5 || ConvChoisie != 6){
+            System.out.println("Erreur dans la saisi de la méthode choisie");
+        }
+        else{
+            System.out.println("Résultat de la conversion: "+ ValeurSortie);
+        }      
     }
 }
