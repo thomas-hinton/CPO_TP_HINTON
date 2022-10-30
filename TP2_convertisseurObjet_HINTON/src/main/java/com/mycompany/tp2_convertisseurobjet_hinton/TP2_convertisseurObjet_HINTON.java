1c630823ed3bf491e1e80ae76ed83b123efc81ba
thomas hinton <thomas.hinton@epfedu.fr>
2022-10-30 13:22:42 +0100

    Création projet "TP2_ConvertisseurObjet"

---
+++ TP2_convertisseurObjet_HINTON/src/main/java/com/mycompany/tp2_convertisseurobjet_hinton/TP2_convertisseurObjet_HINTON.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tp2_convertisseurobjet_hinton;
+
+/**
+ *
+ * @author 33621
+ */
+public class TP2_convertisseurObjet_HINTON {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
